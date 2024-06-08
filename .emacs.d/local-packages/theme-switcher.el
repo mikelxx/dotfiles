22341f29a0ad828bf5ba63d02350b5fa396ede07
Michał Lach <misspelledcocteau@gmail.com>
2024-06-08 23:01:48 +0200

    emacs: add theme-switcher hours customization

--- .emacs.d/local-packages/theme-switcher.el
+++ .emacs.d/local-packages/theme-switcher.el
@@ -17,11 +17,21 @@
   "Default nighttime theme"
   :type '(const :tag 'standard-dark nil))
 
+(defcustom theme-day-switch-hour 8
+  "Time at which the theme is switched to a daytime theme"
+  :group 'theme-switcher
+  :type '(integer))
+
+(defcustom theme-night-switch-hour 20
+  "Time at which the theme is switched to a nighttime theme"
+  :group 'theme-switcher
+  :type '(integer))
+
 (defun load-theme-by-time ()
   "Load set theme based on current time"
   (setq hour (string-to-number (substring (current-time-string) 11 13)))
   (mapcar #'disable-theme custom-enabled-themes)
-  (if (and (< hour 20) (< 8 hour))
+  (if (and (< theme-night-switch-hour 20) (< theme-day-switch-hour hour))
       (load-theme default-day-theme t)
     (load-theme default-night-theme t)))
 
