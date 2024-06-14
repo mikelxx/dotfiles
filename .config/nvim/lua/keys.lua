bfc7d0baf325c30fcc9a83351c585fa7b8dbd4e3
Michał Lach <misspelledcocteau@gmail.com>
2024-06-14 16:30:11 +0200

    nvim: add keybindings for lsp

--- .config/nvim/lua/keys.lua
+++ .config/nvim/lua/keys.lua
@@ -4,3 +4,7 @@ local telescope_builtins = require('telescope.builtin')
 
 vim.keymap.set('n', 'gr', telescope_builtins.live_grep, {})
 vim.keymap.set('n', '<C-p>', telescope_builtins.find_files, {})
+vim.keymap.set('n', 'zo', vim.diagnostic.open_float, { noremap = true, silent = true })
+vim.keymap.set('n', 'zn', vim.diagnostic.goto_next, { noremap = true, silent = true })
+vim.keymap.set('n', 'zp', vim.diagnostic.goto_prev, { noremap = true, silent = true })
+vim.keymap.set('n', '<C-]>', vim.lsp.buf.definition, { noremap = true, silent = true })
